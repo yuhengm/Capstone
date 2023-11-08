01010cb2b02ef24bf5499f5690dc07ad01465801
mengxinchang <lilllmeng@mengxinchangs-MacBook-Pro.local>
2023-11-08 11:08:16 -0800

    split

--- mine_seg_sat/dataset.py
+++ mine_seg_sat/dataset.py
@@ -476,6 +476,96 @@ class MineSATDataset(torch.utils.data.Dataset):
             }
         )
 
+    def safe_divide(self, numerator, denominator):
+        """Safely divide two arrays and handle division by zero."""
+        with np.errstate(divide='ignore', invalid='ignore'):
+            result = numerator / denominator
+            result[~np.isfinite(result)] = 0  # Replace -inf, inf, NaN with 0
+        return result
+
+    def get_images(self, index: int, percentile: int = 95):
+        """
+        Generate Sentinel-2 images from the given filepath. The returned images are as follows:
+
+        NDVI: Normalized Difference Vegetation Index
+        NDBI: Normalized Difference Built-up Index
+        NDWI: Normalized Difference Water Index
+        False Color: B08, B04, B03
+        Mask: The class labels for each pixel
+        """
+        # Unpack the bands
+        image_dict = {}  # Initialize an empty dictionary to store images
+
+        try:
+            filepath = self.filepaths[index]
+            maskpath = self.maskpaths[index]
+            B02 = self.scale_band(
+                tiff.imread(f"{self.data_path.as_posix()}/{filepath}/B02.tif"),
+                percentile=percentile,
+            )
+            B03 = self.scale_band(
+                tiff.imread(f"{self.data_path.as_posix()}/{filepath}/B03.tif"),
+                percentile=percentile,
+            )
+            B04 = self.scale_band(
+                tiff.imread(f"{self.data_path.as_posix()}/{filepath}/B04.tif"),
+                percentile=percentile,
+            )
+            B07 = self.scale_band(
+                tiff.imread(f"{self.data_path.as_posix()}/{filepath}/B07.tif"),
+                percentile=percentile,
+            )
+            B08 = self.scale_band(
+                tiff.imread(f"{self.data_path.as_posix()}/{filepath}/B08.tif"),
+                percentile=percentile,
+            )
+
+            B11 = cv2.resize(
+                tiff.imread(f"{self.data_path.as_posix()}/{filepath}/B11.tif"),
+                None,
+                fx=2,
+                fy=2,
+                interpolation=cv2.INTER_CUBIC,
+            )
+            B12 = cv2.resize(
+                tiff.imread(f"{self.data_path.as_posix()}/{filepath}/B12.tif"),
+                None,
+                fx=2,
+                fy=2,
+                interpolation=cv2.INTER_CUBIC,
+            )
+            B11 = self.scale_band(B11, percentile=percentile)
+            B12 = self.scale_band(B12, percentile=percentile)
+
+            mask = tiff.imread(f"{self.data_path.as_posix()}/{maskpath}/mask.tif")
+            mask = self.colorize_mask(mask)
+
+            # Calculate NDVI (Normalized Difference Vegetation Index)
+            NDVI = self.safe_divide(B08 - B04,  B08 + B04)
+
+
+            # Create a color image using RGB bands
+            RGB = np.stack([B04, B03, B02], axis=-1)
+
+            # Upscale B07 to match the resolution of B04
+            B07_rescaled = cv2.resize(
+                B07, (B04.shape[1], B04.shape[0]), interpolation=cv2.INTER_CUBIC)
+
+            # Now compute NBR
+            NBR = self.safe_divide(B08 - B07_rescaled, B08 + B07_rescaled)
+            # Create a false-color composite image
+            false_color = np.stack([B08, B04, B03], axis=-1)
+
+            # Add to the dictionary
+            image_dict["NBR"] = NBR
+            image_dict["NDVI"] = NDVI
+            image_dict["RGB"] = RGB
+
+        except FileNotFoundError as e:
+            print(f"File not found: {e}")
+            # Additional error handling can be done here if needed
+
+        return image_dict
 
     def downsample(self, image, scale_factor):
 
